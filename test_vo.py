3e69a4aa47132bec9525e7d442a73042970630ce
Amin-Golden <aminabazari@gmail.com>
2022-04-21 17:52:16 +0430

    run on labtop

--- test_vo.py
+++ test_vo.py
@@ -30,21 +30,28 @@ parser.add_argument("--img-exts", default=['png', 'jpg', 'bmp'], nargs='*', type
 parser.add_argument("--rotation-mode", default='euler', choices=['euler', 'quat'], type=str)
 
 parser.add_argument("--sequence", default='09', type=str, help="sequence to test")
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-
-
+device =  torch.device("cpu")
+# torch.device("cuda") if torch.cuda.is_available() else
+vid = cv2.VideoCapture(2)
 def load_tensor_image(filename, args):
-    img = imread(filename).astype(np.float32)
+    # img = imread(filename).astype(np.float32)
+    ret, img_r = vid.read()
+    
+    img=img_r.astype(np.float32)
     h, w, _ = img.shape
     if (not args.no_resize) and (h != args.img_height or w != args.img_width):
         img = imresize(img, (args.img_height, args.img_width)).astype(np.float32)
     img = np.transpose(img, (2, 0, 1))
+    
+
     tensor_img = ((torch.from_numpy(img).unsqueeze(0)/255-0.45)/0.225).to(device)
-    return tensor_img
+    return tensor_img,img_r
 
 
 @torch.no_grad()
+
 def main():
+    
     args = parser.parse_args()
 
     weights_pose = torch.load(args.pretrained_posenet)
@@ -56,21 +63,22 @@ def main():
     output_dir = Path(args.output_dir)
     output_dir.makedirs_p()
 
-    test_files = sum([image_dir.files('*.{}'.format(ext)) for ext in args.img_exts], [])
-    test_files.sort()
+    # test_files = sum([image_dir.files('*.{}'.format(ext)) for ext in args.img_exts], [])
+    # test_files.sort()
 
-    print('{} files to test'.format(len(test_files)))
+    # print('{} files to test'.format(len(test_files)))
 
     global_pose = np.eye(4)
     poses = [global_pose[0:3, :].reshape(1, 12)]
 
-    n = len(test_files)
-    tensor_img1 = load_tensor_image(test_files[0], args)
-
-    for iter in tqdm(range(n - 1)):
-
-        tensor_img2 = load_tensor_image(test_files[iter+1], args)
-
+    # n = len(test_files)
+    test_files="amin"
+    tensor_img1,img = load_tensor_image(test_files, args)
+    counter=0
+    for iter in tqdm(range(500)):
+        counter=counter+1
+        tensor_img2,img = load_tensor_image(test_files, args)
+        
         pose = pose_net(tensor_img1, tensor_img2)
 
         pose_mat = pose_vec2mat(pose).squeeze(0).cpu().numpy()
@@ -81,6 +89,10 @@ def main():
 
         # update
         tensor_img1 = tensor_img2
+        # Display the resulting frame
+        cv2.imshow('frame', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     poses = np.concatenate(poses, axis=0)
     filename = Path(args.output_dir + args.sequence + ".txt")
