473f6a6f5513ee8bb3ec93b4a09dba77b80c0bcb
Amin-Golden <aminabazari@gmail.com>
2022-05-21 17:59:48 +0430

    mask 0

--- scripts/train_resnet50_pose_256.sh
+++ scripts/train_resnet50_pose_256.sh
@@ -5,7 +5,7 @@ python train.py $TRAIN_SET \
 --num-scales 1 \
 -b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 3 \
 --with-ssim 1 \
---with-mask 1 \
+--with-mask 0 \
 --with-auto-mask 1 \
 --with-pretrain 1 \
 --log-output \
