9bb036a8bcda5d7c3d3150e99783530c7f81fba6
Amin-Golden <aminabazari@gmail.com>
2022-05-21 15:41:44 +0430

    change dataset address

--- scripts/run_prepare_data.sh
+++ scripts/run_prepare_data.sh
@@ -1,6 +1,6 @@
 # for kitti raw dataset
-DATASET=/media/bjw/Disk/Dataset/kitti_raw/
-TRAIN_SET=/media/bjw/Disk/Dataset/kitti_256/
+DATASET=../input/kitti-odometry-woo
+TRAIN_SET=/Dataset/kitti_256/
 STATIC_FILES=data/static_frames.txt
 python data/prepare_train_data.py $DATASET --dataset-format 'kitti_raw' --dump-root $TRAIN_SET --width 832 --height 256 --num-threads 4 --static-frames $STATIC_FILES --with-depth 
 
