80c0ed548d4de4c1b85c178107e5e02b7c5f4ffe
Amin-Golden <aminabazari@gmail.com>
2022-05-22 14:02:16 +0430

    train set address

--- scripts/run_prepare_data.sh
+++ scripts/run_prepare_data.sh
@@ -11,5 +11,5 @@
 
 # for kitti odometry dataset
 DATASET=../../input/kitti-odometry-woo
-TRAIN_SET=/Dataset/kitti_vo_256/
+TRAIN_SET=/kaggle/working/Dataset/kitti_vo_256/
 python data/prepare_train_data.py $DATASET --dataset-format 'kitti_odom' --dump-root $TRAIN_SET --width 832 --height 256 --num-threads 4
